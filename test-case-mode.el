d2f2aef9e1fa2ab7e7e61e8ea8c90f7d94a44170
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-28 08:26:05 +0100

    Update README to mention install from melpa

--- test-case-mode.el
+++ test-case-mode.el
@@ -33,12 +33,7 @@
 ;; The back-ends probably need some more path options to work correctly.
 ;; Please let me know, as I'm not an expert on all of them.
 ;;
-;; To install test-case-mode, add the following to your .emacs:
-;; (add-to-list 'load-path "/path/to/test-case-mode")
-;; (autoload 'test-case-mode "test-case-mode" nil t)
-;; (autoload 'enable-test-case-mode-if-test "test-case-mode")
-;; (autoload 'test-case-find-all-tests "test-case-mode" nil t)
-;; (autoload 'test-case-compilation-finish-run-all "test-case-mode")
+;; To install test-case-mode, add the require it from melpa.
 ;;
 ;; To enable it automatically when opening test files:
 ;; (add-hook 'find-file-hook 'enable-test-case-mode-if-test)
